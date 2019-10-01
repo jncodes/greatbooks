cf8ce97a01cf9e082abe03f28a22b9bef3432eb9
jncodes <midn.nelson@gmail.com>
2019-10-01 14:39:12 -0400

    Add Static Pages, Index component, Frontend folders, User model and table

---
+++ app/frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Meet your next favorite book.</h1>, root);
+});
