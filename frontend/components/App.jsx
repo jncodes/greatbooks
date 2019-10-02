6f0865fdc869c86993fe24aa093aa9ff2ef9f6e6
jncodes <midn.nelson@gmail.com>
2019-10-02 11:04:06 -0400

    Add Root and App

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>Meet your next favorite book!</h1>
+    </div>
+);
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from "./components/root";
 
 //test
     // import { signup, login, logout } from './util/session_api_util';
@@ -19,9 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //test
 
-    // const store = configureStore();
+    const store = configureStore();
 
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Meet your next favorite book.</h1>, root);
-    // ReactDOM.render(<Root store={store} />, root);
+    // ReactDOM.render(<h1>Meet your next favorite book.</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
