39ee4a8bd65f200103c2d1e1a67a39729349494f
jncodes <midn.nelson@gmail.com>
2019-10-02 09:31:29 -0400

    Add Session Reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const _nullUser = {
+    id: null
+};
+
+export default (state = _nullUser, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            const { id } = action.user;
+            return Object.assign({}, { id });
+        case LOGOUT_CURRENT_USER:
+            return _nullUser;
+        default:
+            return state;
+    }
+};
